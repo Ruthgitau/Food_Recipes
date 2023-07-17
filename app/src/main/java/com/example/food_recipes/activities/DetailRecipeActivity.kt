e61708920974a9ee5a6e62af34b3f85bcefd3b4b
Ruthgitau <gitaur72@gmail.com>
2023-07-17 13:52:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/food_recipes/activities/DetailRecipeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.food_recipes.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.food_recipes.R
+
+class DetailRecipeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_recipe)
+    }
+}
---
+++ app/src/main/java/com/example/food_recipes/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.food_recipes.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.food_recipes.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
